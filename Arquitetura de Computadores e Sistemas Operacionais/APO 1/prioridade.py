39f538d6800107ad68b8cd3b271612812c0fcffa
KallelGaNewk <kallelgn@gmail.com>
2025-03-12 23:07:47 -0300

    Ajustar prioridade.py para usar milisegundos

--- Arquitetura de Computadores e Sistemas Operacionais/APO 1/prioridade.py
+++ Arquitetura de Computadores e Sistemas Operacionais/APO 1/prioridade.py
@@ -1,45 +1,37 @@
 # prioridade.py
 # Implementação do escalonamento por prioridade de threads em Python
 
-import threading
 import time
 
 # Cria uma Classe para representar uma Thread
-class Thread:
+class Processo:
     def __init__(self, thread_id, tempo, prioridade):
-        # Criamos aqui a Thread da biblioteca threading, mas não a iniciamos
-        self.thread = threading.Thread(target=self.tarefa, args=(thread_id, tempo))
-
         # Definimos aqui os atributos da Thread, para que possamos acessá-los posteriormente
+        self.thread_id = thread_id
         self.tempo = tempo
         self.prioridade = prioridade
 
-    def tarefa(self, thread_id, tempo_execucao):
-        print(f"Thread {thread_id} iniciada, executando por {tempo_execucao} segundos...")
-        time.sleep(tempo_execucao) # Simula o tempo de execução da thread
-        print(f"Thread {thread_id} finalizada!")
+    def tarefa(self):
+        print(f"Thread {self.thread_id} iniciada, executando por {self.tempo}ms...")
+        time.sleep(self.tempo / 1000) # Simula o tempo de execução da thread
+        print(f"Thread {self.thread_id} finalizada!")
+        return self.tempo
 
-# Tempo de execução para cada thread (em segundos)
-tempos_de_execucao = [2, 4, 1, 3, 5]
+# Tempo de execução para cada thread (em milissegundos)
+tempos_de_execucao = [5, 10, 3]
 # Prioridade de cada thread (quanto menor, maior a prioridade)
-prioridades = [3, 1, 5, 2, 0]
+prioridades = [1, 0, 2]
 
 # Cria as threads com base nos tempos de execução e prioridades
-threads = [Thread(i+1, tempo, prioridades[i]) for i, tempo in enumerate(tempos_de_execucao)]
+threads = [Processo(i+1, tempo, prioridades[i]) for i, tempo in enumerate(tempos_de_execucao)]
 
 # Ordena as threads por prioridade
-threads.sort(key=lambda x: x.prioridade, reverse=True)
+threads.sort(key=lambda x: x.prioridade)
 
-start_time = time.time()
+total_time = 0
 
 # Inicia as threads
 for thread in threads:
-    thread.thread.start()
+    total_time += thread.tarefa()
 
-# Espera todas as threads finalizarem
-for thread in threads:
-    thread.thread.join()
-
-end_time = time.time()
-
-print(f"Todas as threads finalizadas em {end_time - start_time} segundos!")
+print(f"Todas as threads finalizadas em {total_time}ms!")
